29df5544a4c95a743717d170555964e5fef86cb1
Sharad Raj <dev_sharad@outlook.com>
2022-05-12 14:34:50 +0000

    FIx

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <section class="container pt-4 ">
-  <h1 class="display-6"><a class="text-decoration-none" href="/" use:link>appwrite <span class="text-muted">| pastebin</span></a></h1>
+  <h1 class="display-6"><a class="text-decoration-none" href="/" use:link>appwrite <span class="text-muted">| AnonPaste</span></a></h1>
   <hr />
 </section>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -3,7 +3,10 @@
   import { navigate } from "svelte-routing";
 </script>
 
+<img src="/hero.png" alt="" srcset="" style="height: 25vw;" />
 <p>Hi! Folks, this app was developed using appwrite project in just a few hours.</p>
+<p>It uses Anonymous login to save the pastes created by the user.</p>
+<pre>PS: It's a pastebin clone</pre>
 <Button
   on:click="{() => {
     navigate('/paste');
